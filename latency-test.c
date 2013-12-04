dd92c14219f4742690407c922dc011818758ee56
Andiry Xu <jix024@cs.ucsd.edu>
2013-12-04 13:55:08 -0800

    Add get_cpuid

--- latency-test.c
+++ latency-test.c
@@ -6,9 +6,29 @@
 #include<string.h>
 #include<malloc.h>
 #include<stdlib.h>
+#include<stdint.h>
 #include<pthread.h>
 #include<sys/mman.h>
 #include<sys/time.h>
+
+#define cpuid(func,ax,bx,cx,dx)\
+        __asm__ __volatile__ ("cpuid":\
+        "=a" (ax), "=b" (bx), "=c" (cx), "=d" (dx) : "a" (func));
+
+static inline int get_cpuid(void)
+{
+	int id;
+	uint32_t eax = 0;
+	uint32_t ebx = 0;
+	uint32_t ecx = 1;
+	uint32_t edx = 0;
+
+	cpuid(0x0B, eax, ebx, ecx, edx);
+
+	id = (((edx & 1) << 3) + ((edx >> 4) & 1) + (edx & 0xe));
+
+	return id;
+}
 
 int main(int argc, char **argv)
 {
@@ -35,8 +55,9 @@ int main(int argc, char **argv)
 	gettimeofday(&begin, &tz);
 	clock_gettime(CLOCK_MONOTONIC, &start);
 	for (i = 0; i < count; i++) {
-		pthread_rwlock_wrlock(&rw_lock);
-		offset += size;
+//		get_cpuid();
+		pthread_rwlock_rdlock(&rw_lock);
+//		offset += size;
 //		__sync_fetch_and_add(&offset, size);
 		pthread_rwlock_unlock(&rw_lock);
 	}
