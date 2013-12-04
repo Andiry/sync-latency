2b957faa3f1709507d710f2068df3136d6e410e2
Andiry Xu <jix024@cs.ucsd.edu>
2013-12-04 15:28:17 -0800

    Using atomic operations to emulate rwlock

--- latency-test.c
+++ latency-test.c
@@ -48,18 +48,33 @@ int main(int argc, char **argv)
 	char *buf;
 	FILE *output;
 	pthread_rwlock_t rw_lock;
+	uint64_t lock[16] = {0};
+	uint64_t wr_lock = 0;
+	int cpu_id;
+	int j, lock_held;
 
 	count = 1000000;
 	pthread_rwlock_init(&rw_lock, NULL);
+	cpu_id = get_cpuid();
+	printf("cpuid: %d\n", cpu_id);
 
 	gettimeofday(&begin, &tz);
 	clock_gettime(CLOCK_MONOTONIC, &start);
 	for (i = 0; i < count; i++) {
-//		get_cpuid();
-		pthread_rwlock_rdlock(&rw_lock);
-//		offset += size;
-//		__sync_fetch_and_add(&offset, size);
-		pthread_rwlock_unlock(&rw_lock);
+		while(__sync_fetch_and_add(&lock[cpu_id], 1) >= (1 << 31))
+			__sync_fetch_and_sub(&lock[cpu_id], 1);
+
+		__sync_fetch_and_sub(&lock[cpu_id], 1);
+#if 0
+		for (j = 0; j < 16; j++) {		
+			while (!__sync_bool_compare_and_swap(&lock[j], 0, 1 << 31))
+				;
+		}
+
+		for (j = 0; j < 16; j++) {		
+			__sync_bool_compare_and_swap(&lock[j], 1 << 31, 0);
+		}
+#endif
 	}
 	clock_gettime(CLOCK_MONOTONIC, &end);
 	gettimeofday(&finish, &tz);
@@ -68,6 +83,7 @@ int main(int argc, char **argv)
 	time1 = (finish.tv_sec - begin.tv_sec) * 1e6 + (finish.tv_usec - begin.tv_usec);
 	printf("%lld times,\t %lld nanoseconds,\t latency %lld nanoseconds.\n", count, time, time / count);
 	printf("Write process %lld microseconds\n", time1);
+	printf("wr_lock: %lld, rw_lock: %lld\n", wr_lock, lock[0]);
 
 	return 0;
 }
